4dade84cfa3ec434f84bb77c3057b3fc7791003c
annisachrkt <annisacchairani@gmail.com>
2025-05-30 12:42:54 +0700

    menambah model, mengambil dan menghapus data dari API

---
+++ nisa-rest-server/application/controllers/api/mahasiswa.php
@@ -0,0 +1,75 @@
+<?php
+
+use RestServer\Libraries\REST_Controller;
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH . 'libraries/REST_Controller.php';
+require APPPATH . 'libraries/Format.php';
+
+
+
+class mahasiswa extends REST_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('Mahasiswa_model', 'mahasiswa');
+
+       
+    }
+
+    public function index_get() {
+        
+        $id = $this->get('id');
+        if ($id == null) {
+            $mahasiswa = $this->mahasiswa->getMahasiswa();
+        } else {
+            $mahasiswa = $this->mahasiswa->getMahasiswa($id);
+        } 
+
+        if ($mahasiswa) {
+            $this->response([
+                    'status' => true,
+                    'data' => $mahasiswa
+                ], REST_Controller::HTTP_OK);
+        } else {
+            $this->response([
+                    'status' => false,
+                    'message' => 'id not found'
+                ], REST_Controller::HTTP_NOT_FOUND);
+        }
+    }
+    public function index_delete() {
+    $id = $this->delete('id');
+
+    if ($id == null) {
+        $this->response([
+            'status' => false,
+            'message' => 'provide an id'
+        ], REST_Controller::HTTP_BAD_REQUEST);
+    } else {
+        
+        if ($id > 0) {
+            if ($this->mahasiswa->deleteMahasiswa($id) > 0) {
+                $this->response([
+                    'status' => true,
+                    'id' => $id,
+                    'message' => 'Mahasiswa has been deleted.'
+                ], REST_Controller::HTTP_OK);
+            } else {
+                $this->response([
+                    'status' => false,
+                    'message' => 'ID not found'
+                ], REST_Controller::HTTP_BAD_REQUEST);
+            }
+        } else {
+            $this->response([
+                'status' => false,
+                'message' => 'ID must be greater than 0'
+            ], REST_Controller::HTTP_BAD_REQUEST);
+        }
+    }
+}
+
+
+}
+
